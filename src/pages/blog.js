2be5f3201e1eaadffdc9a0c3bc575750fa1bcf41
Radosław Adamczyk <greatpolandweb@gmail.com>
2022-01-18 16:29:49 +0100

    finish blog page

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -4,29 +4,32 @@ import TitlePage from '../styles/Titles/TitlePage';
 import Text from '../styles/Text/Text';
 import ArticleCard from '../components/atoms/HomePage/ArticleCard';
 import {graphql} from 'gatsby';
+import BlogWrapper from '../styles/Containers/BlogWrapper';
 
 const BlogPage = ({data}) => {
     return(
         <Layout>
-            <TitlePage>
-                Blog
-            </TitlePage>
-            <Text>
-                Distinctio inventore est. Dignissimos aliquam debitis nihil at quasi optio dolores. Quisquam corporis quia neque sit vel dolorum optio. Ad enim dolor deleniti ullam quaerat necessitatibus ipsa exercitationem aliquam. Et consequuntur totam dolorem. Repudiandae nam repellendus iusto doloremque blanditiis rerum voluptatum ducimus sint.Veniam minima non et perferendis quia ad distinctio perferendis. Ratione eaque sit non. Est consequuntur voluptates eos cum. Quas necessitatibus odio asperiores. Cupiditate in ut in ducimus placeat voluptates expedita dicta omnis. Quo commodi corrupti consequatur. Officiis repudiandae esse est reiciendis adipisci.
-            </Text>
-            <div>
-                {
-                    data.allDatoCmsArticle.edges.map(({node}) => 
-                        <ArticleCard
-                            title={node.title}
-                            excerpt={node.excerpt}
-                            path={'/blog/' + node.slug}
-                        />
-                    )
-                }
-
-                
-            </div>
+            <BlogWrapper>
+                <div className='content'>
+                    <TitlePage>
+                        Blog
+                    </TitlePage>
+                    <Text>
+                        Distinctio inventore est. Dignissimos aliquam debitis nihil at quasi optio dolores. Quisquam corporis quia neque sit vel dolorum optio. Ad enim dolor deleniti ullam quaerat necessitatibus ipsa exercitationem aliquam. Et consequuntur totam dolorem. Repudiandae nam repellendus iusto doloremque blanditiis rerum voluptatum ducimus sint.Veniam minima non et perferendis quia ad distinctio perferendis. Ratione eaque sit non. Est consequuntur voluptates eos cum. Quas necessitatibus odio asperiores. Cupiditate in ut in ducimus placeat voluptates expedita dicta omnis. Quo commodi corrupti consequatur. Officiis repudiandae esse est reiciendis adipisci.
+                    </Text>
+                </div>
+                <div className='cards'>
+                    {
+                        data.allDatoCmsArticle.edges.map(({node}) => 
+                            <ArticleCard
+                                title={node.title}
+                                excerpt={node.excerpt}
+                                path={'/blog/' + node.slug}
+                            />
+                        )
+                    }
+                </div>
+            </BlogWrapper>
         </Layout>
     )
 }
