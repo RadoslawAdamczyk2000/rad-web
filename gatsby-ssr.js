d22d080d8f45063cfee754b2726472e20861d5bd
Radosław Adamczyk <greatpolandweb@gmail.com>
2022-01-24 19:17:48 +0100

    ........

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,12 +1,11 @@
 const React = require('react');
 const {ThemeProvider} = require('styled-components');
 const {default: theme} = require('./src/styles/theme');
-const {default: Global} = require('./src/styles/Global');
 
 exports.wrapRootElement = ({element}) => {
     return(
-        <>
-            <Global/>
-        </>
+        <ThemeProvider theme={theme}>
+            {element}
+        </ThemeProvider>
     )
 }
