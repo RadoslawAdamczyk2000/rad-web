c0c74311255f4fdccdc1617d51a141597059a1b2
Radosław Adamczyk <greatpolandweb@gmail.com>
2022-01-24 18:48:44 +0100

    remove layout component

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,14 +1,11 @@
 const React = require('react');
 const {ThemeProvider} = require('styled-components');
 const {default: theme} = require('./src/styles/theme');
-const { default: Layout } = require('./src/templates/Layout');
 
 exports.wrapRootElement = ({element}) => {
     return(
         <ThemeProvider theme={theme}>
-            <Layout>
-                {element}
-            </Layout>
+            {element}
         </ThemeProvider>
     )
 }
