ba848af05b00fe6ff60f22ccee4fc3da66b02c41
Radosław Adamczyk <greatpolandweb@gmail.com>
2022-01-24 18:57:48 +0100

    import global to ssr and browser

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,11 +1,12 @@
 const React = require('react');
 const {ThemeProvider} = require('styled-components');
 const {default: theme} = require('./src/styles/theme');
+const {default: Global} = require('./src/styles/Global');
 
 exports.wrapRootElement = ({element}) => {
     return(
-        <ThemeProvider theme={theme}>
-            {element}
-        </ThemeProvider>
+        <>
+            <Global/>
+        </>
     )
 }
