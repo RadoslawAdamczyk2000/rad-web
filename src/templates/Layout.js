e1aaaf54620ddbcd4bcb6271edb2158a4681979d
Radosław Adamczyk <greatpolandweb@gmail.com>
2022-01-27 10:50:57 +0100

    new post about netflix app step 1

--- src/templates/Layout.js
+++ src/templates/Layout.js
@@ -1,11 +1,9 @@
 import React from 'react';
-// import { ThemeProvider } from 'styled-components';
 import Head from '../components/atoms/Layout/Head';
 import Footer from '../components/organism/Layout/Footer';
 import Navigation from '../components/organism/Layout/Navigation';
 import Wrapper from '../styles/Containers/Wrapper';
 import Global from '../styles/Global';
-// import theme from '../styles/theme';
 
 const Layout = ({children, isArticle, metaDescription, metaRobots, metaTitle, canonical})  => {
     return(
